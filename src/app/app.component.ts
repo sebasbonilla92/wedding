1fcb018f966d6e8f6c572c6dfdf36f1688107321
sebasbonilla92 <sebastian-bonilla@hotmail.com>
2026-08-30 16:36:09 -0500

    mobile menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Gabriella & Sebastian';
+  mobileMenuOpen = false;
+
+  toggleMobileMenu(): void {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+
+  closeMobileMenu(): void {
+    this.mobileMenuOpen = false;
+  }
 }
